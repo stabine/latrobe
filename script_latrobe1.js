033fa4f5415b599d70e0c12547bc61a5dd0b7f50
stabine <133258973+stabine@users.noreply.github.com>
2025-03-18 10:40:27 +0100

    maker adaption and layer control

--- script_latrobe1.js
+++ script_latrobe1.js
@@ -26,7 +26,7 @@ const map1 = L.map('map1', {
   center: [16.737222, -22.936111], // set to Gravesend
   zoom: 3,
   minZoom: 3,  //preset zoom-span 
-  maxZoom: 11
+  maxZoom: 8
 }); 
 
 // osm layer (baselayer)
@@ -39,14 +39,13 @@ osm1.addTo(map1);
 
 
 // Initialisation of Layer for all Markers ???
-const allMarkersLayer = L.layerGroup(); // Layer for all Markers
+const allMarkersLayer1 = L.layerGroup(); // Layer for all Markers
 
 // Create LayerGroups for each certanty-type ???
-const certaintyLayers = {
-    'Select All': allMarkersLayer, // Der kombinierte Layer für alle Marker
-    'Certain Waypoint': L.layerGroup(),
-    'Less Certain Waypoint': L.layerGroup(),
-    'Uncertain Waypoint': L.layerGroup(),
+const roleLayers1 = {
+    'Select All': allMarkersLayer1, // combined Layer for all markers
+    'Visited': L.layerGroup(),
+    'Visited nearby': L.layerGroup(),
   };
 
 
@@ -70,34 +69,36 @@ fetch('latrobe1.json')
       const certainty1 = feature.properties.certainty;
       const time1 = feature.time;
       
-      
-      let color = "green";
-      let opacity;
+      let radius1 = 35;
+      let color1 = "green";
+      let opacity1 = 1;
       
 
-      // adjust size and opacity based on level of certainty
+      // adjust size and opacity based on level of certainty and role
       switch (certainty1) {
         case 'certain':
-          color = "green";
+          color1 = "green";
           break;
         case 'less-certain':
-          color = "yellow";
+          color1 = "yellow";
           break;
         case 'uncertain':
-          color = "red";
+          color1 = "red";
           break;
       }
 
 
       switch(role1) {
         case 'visited':
-          opacity = 1; // opaque
+          radius1 = 800;
+          opacity1 = 1; // opaque
           break;
         case 'visited_nearby':
-          opacity = 0.4; // slightly translucent
+          radius1 = 15000
+          opacity1 = 0.4; // slightly translucent
           break;
         default:
-          opacity = 1; // default opacity
+          opacity1 = 1; // default opacity
       }
 
       
@@ -129,65 +130,41 @@ fetch('latrobe1.json')
         </div>
       `;
 
-      // ---------------------Function to calculate radius based on Zoom level-----------------------
-      function getMarkerRadius(role, zoomLevel) {
-        let baseRadius = 15;  // Default radius for the marker
-
-        // Adjust base size according to 'role'
-        switch(role) {
-          case 'visited':
-            baseRadius = 15;
-            break;
-          case 'visited_nearby':
-            baseRadius = 35;
-            break;
-          default:
-            baseRadius = 15;
-            break;
-        }
-
-        // Adjust size based on the zoom level (e.g. more zoom -> bigger marker)
-        return baseRadius * (zoomLevel / 15);
-      }
+      
 
       // ---------------Create circle marker for each waypoint--------------------
-      const initialRadius = getMarkerRadius(role1, map1.getZoom());
-
-      const marker1 = L.circleMarker([lat1,lng1], { 
-        radius: initialRadius,
-        color: color,
-        opacity: opacity,
-        fillOpacity: opacity
+      
+      const marker1 = L.circle([lat1,lng1], { 
+        radius: radius1,
+        color: color1,
+        opacity: opacity1,
+        fillOpacity: opacity1
       })
         .bindPopup(popupContent1) // Popup when clicking
         .bindTooltip(title1, { permanent: false, direction: "top", offset: [0, -10], className: 'custom-tooltip' }); // Tooltip when hovering;
 
     // Add Marker to allMarkersLayer (for "Select All")
-    allMarkersLayer.addLayer(marker1);
+    allMarkersLayer1.addLayer(marker1);
 
       
 
       // -----------------------------Add marker to the appropriate LayerGroup based on Certainty-type-------------------------------
-      switch (certainty1) {
-        case 'certain':
-          certaintyLayers['Certain Waypoint'].addLayer(marker1);
+      switch (role1) {
+        case 'visited':
+          roleLayers1['Visited'].addLayer(marker1);
           break;
-        case 'less-certain':
-          certaintyLayers['Less Certain Waypoint'].addLayer(marker1);
-          break;
-        case 'uncertain':
-          certaintyLayers['Uncertain Waypoint'].addLayer(marker1);
-          break;
+        case 'visited_nearby':
+          roleLayers1['Visited nearby'].addLayer(marker1);
+          break;        
       }
     });
       
 
   // set "Select All"-Layer as default
-    map1.addLayer(allMarkersLayer);
+    map1.addLayer(allMarkersLayer1);
 
-    map1.addLayer(certaintyLayers['Certain Waypoint']);
-    map1.addLayer(certaintyLayers['Less Certain Waypoint']);
-    map1.addLayer(certaintyLayers['Uncertain Waypoint']);
+    map1.addLayer(roleLayers1['Visited']);
+    map1.addLayer(roleLayers1['Visited nearby']);
     
 
     // Layer control for switching between base maps and overlay layers
@@ -196,14 +173,14 @@ fetch('latrobe1.json')
     };
 
     // -----------------Create and add layer control to the map (still not sure wether I need it)--------------------------------------
-    L.control.layers(baseMaps, certaintyLayers, {
+    L.control.layers(baseMaps, roleLayers1, {
       position: 'topright'
     }).addTo(map1);
 
     //----------------------------- Update marker radius and position when zooming or panning--------------------------------------------------------------
     function updateMarker() {
-      allMarkersLayer.eachLayer(function(marker) {
-        marker.setLatLng(marker.getLatLng()); // Re-set position to update it correctly
+      allMarkersLayer1.eachLayer(function(marker1) {
+        marker1.setLatLng(marker1.getLatLng()); // Re-set position to update it correctly
       });
     }
 
@@ -217,10 +194,10 @@ fetch('traderoute.json')
   .then(response => response.json())
   .then(data2 => {
 
-  // GeoJSON laden
+  // load GeoJSON
   console.log(data2);
 
-  // Polyline direkt aus GeoJSON
+  // polyline from GeoJSON
     L.geoJSON(data2, {
       style: function () {
         return {
@@ -254,6 +231,17 @@ var osm2 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 osm2.addTo(map2);
 
+// Initialisation of Layer for all Markers ???
+const allMarkersLayer2 = L.layerGroup(); // Layer for all Markers
+
+// Create LayerGroups for each certainty-type ???
+const certaintyLayers2 = {
+    'Select All': allMarkersLayer2, // combined Layer for all markers
+    'Certain Waypoint': L.layerGroup(),
+    'Less certain Waypoint': L.layerGroup(),
+    'Uncertain Waypoint': L.layerGroup(),
+};
+
 fetch('latrobe2.json')
   .then(response => response.json())
   .then(data3 => {
@@ -275,9 +263,6 @@ fetch('latrobe2.json')
     
 
     // set markers and popup
-
-    
-
       const popupContent2 = `
           <h3>${title2}</h3>
           <div class="info-group">
@@ -303,59 +288,248 @@ fetch('latrobe2.json')
           </div>
         `;
 
-      // Berechnung der Markergröße und Opazität basierend auf dem Certainty-Level
-      let radius = 25; // Standardgröße
-      let opacity = 1; // Standardopazität
-      let color = "green";
+      // set marker based on certainty level 
+      let radius2 = 25; // default
+      let opacity2 = 1; // default
+      let color2 = "green";
 
       switch(certainty2) {
         case 'certain':
-          radius = 5; // Größer für 'certain'
-          opacity = 1;
-          color = "green";
+          radius2 = 5; 
+          opacity2 = 1;
+          color2 = "green";
           break;
         case 'less-certain':
-          radius = 20; // Kleiner für 'less-certain'
-          opacity = 0.6;
-          color = "yellow";
+          radius2 = 20; 
+          opacity2 = 0.6;
+          color2 = "yellow";
           break;
         case 'uncertain':
-          radius = 30; // Noch kleiner für 'uncertain'
-          opacity = 0.4;
-          color = "red";
+          radius2 = 30; 
+          opacity2 = 0.4;
+          color2 = "red";
           break;
       }
 
     // create marker, select opacity and size
     const marker2 = L.circleMarker([lat2, lng2],{
-      radius: radius,
-      color: 'green',
-      opacity: opacity,
-      fillOpacity: opacity
+      radius: radius2,
+      color: color2,
+      opacity: opacity2,
+      fillOpacity: opacity2
     })
       .bindPopup(popupContent2) // Popup when clicking
       .bindTooltip(title2, { permanent: false, direction: "top", offset: [0, -10], className: 'custom-tooltip' });
 
-    //add marker to map   
-    marker2.addTo(map2)
 
-    // get coordinates for polyline
-    latlngs.push([lat2, lng2]);
+    // Add Marker to allMarkersLayer (for "Select All")
+    allMarkersLayer2.addLayer(marker2);
+
+      
+
+    // -----------------------------Add marker to the appropriate LayerGroup based on Certainty-type-------------------------------
+      switch (certainty2) {
+        case 'certain':
+          certaintyLayers2['Certain Waypoint'].addLayer(marker2);
+          break;
+        case 'less-certain':
+          certaintyLayers2['Less certain Waypoint'].addLayer(marker2);
+          break;
+        case 'uncertain':
+          certaintyLayers2['Uncertain Waypoint'].addLayer(marker2);
+      }
+    
+      // get coordinates for polyline
+      latlngs.push([lat2, lng2]);  
+
+    });
+
+    // create polyline with collected coordinates
+    if (latlngs.length > 1) {  // to ensure more than 1 point
+      L.polyline(latlngs, { color: '#50052b', weight: 2 }).addTo(map2);
+    }
+
+    // set "Select All"-Layer as default
+    map2.addLayer(allMarkersLayer2);
+
+    map2.addLayer(certaintyLayers2['Certain Waypoint']);
+    map2.addLayer(certaintyLayers2['Less certain Waypoint']);
+    map2.addLayer(certaintyLayers2['Uncertain Waypoint']);
+
+    // Layer control for switching between base maps and overlay layers
+    const baseMap2 = {
+      "OpenStreetMap": osm2 //naming the variables mit strings; add ',' after 'osm' when using more map-layers
+    };
+
+    // -----------------Create and add layer control to the map (still not sure wether I need it)--------------------------------------
+    L.control.layers(baseMap2, certaintyLayers2, {
+      position: 'topright'
+    }).addTo(map2);
+
   
+    
+  })
+  .catch(err => console.error('Fehler beim Laden der GeoJSON-Datei:', err));
+
+
+
+  // -------------------Map3 initialization, pre-set location Cape Colony; topografical map------------------------------
+
+  const map3 = L.map('map3',{
+    center: [-33.775120570267205, 22.333937355866688],
+    zoom: 8,
+    minZoom: 8,  //preset zoom-span 
+    maxZoom: 13
   });
-
-  // create polyline with collected coordinates
-  if (latlngs.length > 1) {  // Stelle sicher, dass es mehr als einen Punkt gibt
-    L.polyline(latlngs, { color: 'green' }).addTo(map2);
-  }
-})
-.catch(err => console.error('Fehler beim Laden der GeoJSON-Datei:', err));
-
-  // -------------------Map initialization, pre-set location Cape Colony; topografical map------------------------------
-  const map3 = L.map('map3').setView([-33.775120570267205, 22.333937355866688 ], 8);
 
   // OpenTopoMap layer (baselayer3)
   var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
   OpenTopoMap.addTo(map3);
+
+  // Initialisation of Layer for all Markers ???
+  const allMarkersLayer3 = L.layerGroup(); // Layer for all Markers
+
+  // Create LayerGroups for each certainty-type ???
+  const certaintyLayers3 = {
+      'Select All': allMarkersLayer3, // combined Layer for all markers
+      'Certain Waypoint': L.layerGroup(),
+      'Less certain Waypoint': L.layerGroup(),
+      'Uncertain Waypoint': L.layerGroup(),
+  };
+
+  fetch('latrobe3.json')
+  .then(response => response.json())
+  .then(data4 => {
+
+    const latlngs = [];
+
+    // iterate through GeoJSON to creat points of polyline
+    data4.features.forEach(feature => {
+      const [lng3, lat3] = feature.geometry.coordinates;
+      
+   // setting properties as constantes 
+      const title3 = feature.properties.title;
+      const note3 = feature.properties.note;
+      const role3 = feature.properties.role;
+      const wasDerivedFrom3 = feature.properties.wasDerivedFrom;
+      const closeMatch3 = feature.properties.closeMatch;
+      const certainty3 = feature.properties.certainty;
+      const time3 = feature.time;
+    
+
+    // set markers and popup
+
+    
+
+      const popupContent3 = `
+          <h3>${title3}</h3>
+          <div class="info-group">
+            <p class="subtitle">Role: ${role3}</p>
+            <p><strong>References:</strong><br><a href="${closeMatch3[0]}" target="_blank">GeoNames</a>
+            <br><a href="${closeMatch3[1]}" target="_blank">WikiData</a></p>
+          </div>
+
+          <div class="info-group">
+            <p><strong>Certainty:</strong> <br>${certainty3}<br><br>
+            <strong>Role:</strong> <br>${role3}</p>
+          </div>
+
+          <div class="info-group">
+            <h3>Notes:</h3>
+            <p>${note3}</p>
+          </div>
+
+          <div class="info-group">
+            <p><strong>Coordinates:</strong> [${lng3}, ${lat3}]</p>
+            <p><strong>Date:</strong> ${time3}</p>
+            ${wasDerivedFrom3 ? `<p><strong>Source:</strong><br><a href="${wasDerivedFrom3[1]}" target="blank">${wasDerivedFrom3[0]}</a>`: ''}
+          </div>
+        `;
+
+      // Berechnung der Markergröße und Opazität basierend auf dem Certainty-Level
+      let radius3 = 100; // Standardgröße
+      let opacity3 = 1; // Standardopazität
+      let color3 = "green";
+
+      switch(certainty3) {
+        case 'certain':
+          radius3 = 100; // Größer für 'certain'
+          opacity3 = 1;
+          color3 = "green";
+          break;
+        case 'less-certain':
+          radius3 = 3000; // Kleiner für 'less-certain'
+          opacity3 = 0.6;
+          color3 = "yellow";
+          break;
+        case 'uncertain':
+          radius3 = 3000; // Noch kleiner für 'uncertain'
+          opacity3 = 0.4;
+          color3 = "red";
+          break;
+      }
+
+    // create marker, select opacity and size
+    const marker3 = L.circle([lat3, lng3],{
+      radius: radius3,
+      color: color3,
+      opacity: opacity3,
+      fillOpacity: opacity3
+    })
+      .bindPopup(popupContent3) // Popup when clicking
+      .bindTooltip(title3, { permanent: false, direction: "top", offset: [0, -10], className: 'custom-tooltip' });
+
+
+    // Add Marker to allMarkersLayer (for "Select All")
+    allMarkersLayer3.addLayer(marker3);
+
+      
+
+    // -----------------------------Add marker to the appropriate LayerGroup based on Certainty-type-------------------------------
+      switch (certainty3) {
+        case 'certain':
+          certaintyLayers3['Certain Waypoint'].addLayer(marker3);
+          break;
+        case 'less-certain':
+          certaintyLayers3['Less certain Waypoint'].addLayer(marker3);
+          break;
+        case 'uncertain':
+          certaintyLayers3['Uncertain Waypoint'].addLayer(marker3);
+      }
+
+    
+
+    // get coordinates for polyline
+    latlngs.push([lat3, lng3]);
+  
+  });
+
+  // create polyline with collected coordinates
+  if (latlngs.length > 1) {  // Stelle sicher, dass es mehr als einen Punkt gibt
+    L.polyline(latlngs, { 
+      color: '#50052b',
+      weight: 3 
+    })
+    .addTo(map3);
+  }
+
+  // set "Select All"-Layer as default
+  map3.addLayer(allMarkersLayer3);
+
+  map3.addLayer(certaintyLayers3['Certain Waypoint']);
+  map3.addLayer(certaintyLayers3['Less certain Waypoint']);
+  map3.addLayer(certaintyLayers3['Uncertain Waypoint']);
+
+  // Layer control for switching between base maps and overlay layers
+  const baseMap3 = {
+    "OpenTopoMap": OpenTopoMap //naming the variables mit strings; add ',' after 'osm' when using more map-layers
+  };
+
+  // -----------------Create and add layer control to the map (still not sure wether I need it)--------------------------------------
+  L.control.layers(baseMap3, certaintyLayers3, {
+    position: 'topright'
+  }).addTo(map3);
+})
+.catch(err => console.error('Fehler beim Laden der GeoJSON-Datei:', err));
