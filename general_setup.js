da11ad2cd3db4a87d9554ab71e3f5fc10802e492
stabine <133258973+stabine@users.noreply.github.com>
2025-05-13 10:26:41 +0200

    update license

--- general_setup.js
+++ general_setup.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright (C) 2025 Max Mustermann
+ * Copyright (C) 2025 Sabine Hollmann
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, Version 3,
